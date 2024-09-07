1c6e5c975f55af12d91f7f5cd8ba2a06a15cd67c
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2024-09-07 21:35:52 +0900

    Update GetMutterListLogic.java

--- src/main/java/model/GetMutterListLogic.java
+++ src/main/java/model/GetMutterListLogic.java
@@ -5,9 +5,9 @@ import java.util.List;
 import dao.MuttersDAO;
 
 public class GetMutterListLogic {
-	 public List<Mutter> execute() {
-		  MuttersDAO dao = new MuttersDAO();
-		  List<Mutter> mutterList = dao.findAll();
-		  return mutterList;
-	 }
+  public List<Mutter> execute() {
+    MuttersDAO dao = new MuttersDAO();
+    List<Mutter> mutterList = dao.findAll();
+    return mutterList;
+  }
 }
